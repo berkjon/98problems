6f29f70df7b43e1c2c96b7b4de8fa20e2ae4432f
Jonathan Berk <jdsberk@gmail.com>
2015-03-30 16:43:31 -0700

    work in process refactoring to remove UserTrack table

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,12 +1,17 @@
 class Track < ActiveRecord::Base
   has_many :playlist_tracks
   has_many :track_tags
-  has_many :user_tracks
 
   has_many :users, through: :playlists
-  has_many :users, through: :user_tracks
 
   has_many :playlists, through: :playlist_tracks
   has_many :tags, through: :track_tags
+
+  after_create :add_to_library
+
+  def add_to_library
+    library_playlist = Playlist.where(name: 'Library').first
+    PlaylistTrack.create(playlist_id: library_playlist, track_id: self.id)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   has_many :user_playlists
   has_many :playlists, through: :user_playlists
 
-  has_many :user_tracks
-  has_many :tracks, through: :user_tracks
   has_many :tracks, through: :playlists
   has_many :tags, through: :tracks
   has_many :tags, through: :playlists

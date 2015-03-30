7cdcdf8307ba4c3c12bdf2d5f5d3af1569389025
Jonathan Berk <jdsberk@gmail.com>
2015-03-30 00:17:26 -0700

    WIP database structure - exploring multiple 'has_many through' relationships

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,8 +1,11 @@
 class Track < ActiveRecord::Base
   has_many :playlist_tracks
   has_many :track_tags
+  has_many :user_tracks
 
   has_many :users, through: :playlists
+  has_many :users, through: :user_tracks
   has_many :playlists, through: :playlist_tracks
   has_many :tags, through: :track_tags
+
 end

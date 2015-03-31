ce93e6580b209b8793bfe59ea8956a48380b5780
Jonathan Berk <jdsberk@gmail.com>
2015-03-30 19:43:31 -0700

    made seed file; last commit before rebuilding DB following discussion with Rao

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,15 @@
 # 5.times {User.create(display_name: Faker::Name.name)}
 # User.all.each {|user| user.playlists.create(name: Faker::Name.)}
+
+5.times do
+  user = User.create(display_name: Faker::Name.name)
+  4.times do
+    playlist = user.playlists.create(name: Faker::Hacker.noun)
+    5.times do
+      track = playlist.tracks.create(name: Faker::App.name)
+      5.times do
+        tag = track.tags.create(tag: Faker::Commerce.color)
+      end
+    end
+  end
+end

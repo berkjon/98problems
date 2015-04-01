4e7213086027956f99c9d277ae521cf4a7313ada
Jonathan Berk <jdsberk@gmail.com>
2015-04-01 00:20:25 -0700

    WIP ajax/jquery to add tag; almost there but can't isolate specific TD to append tag

--- public/js/tracks.js
+++ public/js/tracks.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-
-  $
-
+  bindEvents();
 });
+
+function bindEvents(){
+  $('button.add-tag').on('click', addTag);
+  // get rid of clicks and just do based on 'enter' instead
+  // add functionality for deleting tags
+}
+
+function addTag(event){
+  event.preventDefault();
+  // debugger;
+  var user_id = $(this).parent().data('userId')
+  var track_id = $(this).parent().data('trackId')
+  $.ajax({
+    type: 'post',
+    url: "/users/"+user_id+"/tracks/"+track_id+"/tags/add",
+    data: {tag: $('input').val()},
+  }).done(function(){
+  debugger;
+    appendTag($('input').val());
+  });
+}
+
+function appendTag(tag, location){
+  $('td.tags').append(tag);
+}

3e09342e60bf47c0b2b92b8cfc608585823b78d6
Jonathan Berk <jdsberk@gmail.com>
2015-04-02 16:23:38 -0700

    add and delete tags for a song, fully functional, with ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -37,10 +37,20 @@ post '/users/:user_id/tracks/:track_id/tags/add' do
   current_tag = current_user.tags.find_or_create_by(name: tag_string)
   current_tag.link_to_usertrack(current_user.id, params[:track_id])
 
-  redirect "/users/#{params[:user_id]}" unless request.xhr?
+  if request.xhr?
+    content_type :json
+    {user_id: params[:user_id], track_id: params[:track_id], tag_id: current_tag.id}.to_json
+    # erb :_tag, locals: {tag: tag}, layout: false
+  else
+    redirect "/users/#{params[:user_id]}"
+  end
 end
 
-
+delete '/users/:user_id/tracks/:track_id/tags/:tag_id/remove' do
+  usertrack_id = UserTrack.where(user_id: params[:user_id], track_id: params[:track_id]).first.id
+  TagUsertrack.where(tag_id: params[:tag_id], user_track_id:usertrack_id).first.destroy
+  redirect "/users/#{params[:user_id]}" unless request.xhr?
+end
 
 
 get '/clear_session' do
--- public/js/tracks.js
+++ public/js/tracks.js
@@ -4,34 +4,60 @@ $(document).ready(function() {
 
 function bindEvents(){
   // $('button.add-tag').on('click', addTag);
-  $('form').submit(addTag);
+  $('form.add-tag-form').submit(addTag);
+  $('form.remove-tag-form').submit(removeTag);
   // get rid of clicks and just do based on 'enter' instead
   // add functionality for deleting tags
 }
 
 function addTag(event){
   event.preventDefault();
-  var user_id = event.target.dataset.userId
-  var track_id = event.target.dataset.trackId
+  var user_id = event.target.parentElement.parentElement.dataset.userId
+  var track_id = event.target.parentElement.parentElement.dataset.trackId
   // var user_id = $(this).parent().data('userId') // BETTER TO USE 'THIS' OR 'EVENT' TO PULL DATA?
   // var track_id = $(this).parent().data('trackId')
-  var input_value = event.target.querySelector('[name="tag"]').value
+  var newTagString = event.target.querySelector('[name="tag"]').value
   var tag_element = event.target.parentElement.parentElement.querySelector('td.tags')
-  debugger;
-  appendTag(input_value, tag_element);
+
+  $(tag_element).append(formatNewTag(newTagString))
   event.target.reset();
   $.ajax({
     type: 'post',
     url: "/users/"+user_id+"/tracks/"+track_id+"/tags/add",
-    data: {tag: input_value},
-  }).done(function(){
-    console.log("SUCCESS! Added tag '"+input_value+"' to user #"+user_id+"'s tags for track #"+track_id+".")
+    data: {tag: newTagString},
+  }).done(function(response){
+    console.log("SUCCESS! Added tag '"+newTagString+"' to user #"+user_id+"'s tags for track #"+track_id+".")
+    updateDataTagId(response)
   }).fail(function(){
-    console.log("FAILED to add tag '"+input_value+"' to user #"+user_id+"'s tags for track #"+track_id+".")
-    alert("Something went wrong - please try again")
+    console.log("FAILED to add tag '"+newTagString+"' to user #"+user_id+"'s tags for track #"+track_id+".")
+    alert("Failed to add tag - please try again")
   });
 }
 
-function appendTag(tag, location){
-  $(location).append(tag);
+function removeTag(event){
+  event.preventDefault();
+  var user_id = $(event.target).parents().eq(2).data('userId')
+  var track_id = $(event.target).parents().eq(2).data('trackId')
+  var tag_id = $(event.target).data('tagId')
+  $(event.target).parent().remove();
+  $.ajax({
+    type: 'delete',
+    url: "/users/"+user_id+"/tracks/"+track_id+"/tags/"+tag_id+"/remove",
+  }).done(function(){
+    console.log("SUCCESS! Removed tag #"+tag_id+" from user #"+user_id+"'s tags for track #"+track_id+".")
+  }).fail(function(){
+    console.log("FAILED to remove tag #"+tag_id+" from user #"+user_id+"'s tags for track #"+track_id+".")
+    alert("Failed to remove tag - please try again")
+  });
+};
+
+function formatNewTag(newTagString){
+  return "<button type='button' class='tag'><form data-tag-id='_TAG_ID_' class='remove-tag-form' action='/users/_USER_ID_/tracks/_TRACK_ID_/tags/_TAG_ID_/remove' method='post'><input name='_method' type='hidden' value='delete'><input type='submit' value='&#10006' class='remove-tag'>"+newTagString+"</form></button>"
+};
+
+function updateDataTagId(response){
+  var form = $("[data-track-id="+response.track_id+"]").find("[data-tag-id=_TAG_ID_]")
+  form.data('tagId', response.tag_id)
+  form.attr('action', "/users/"+response.user_id+"/tracks/"+response.track_id+"/tags/"+response.tag_id+"/remove")
+  console.log("Successfully updated hidden properties of new tag")
 }
